f77faa89c642cc777a9ab62d19b0bb243ef76f1a
MichaelZp <michael.zappe@yahoo.de>
2020-02-01 02:40:30 +0100

    Search text retains after search. Added button for delayed search

--- WebApp/Controllers/SearchController.cs
+++ WebApp/Controllers/SearchController.cs
@@ -11,28 +11,38 @@ namespace WebApp.Controllers
         // 
         // GET: / 
 
-        async public Task<IActionResult> Index(string name)
+        async private Task<IActionResult> Search(string name)
         {
             SearchModel model = new SearchModel();
 
-            if(string.IsNullOrEmpty(name))
+            if (string.IsNullOrEmpty(name))
             {
                 ViewData["Error"] = "";
                 return View(model.Players);
             }
 
 
+
             var success = await model.FillPlayers(name);
 
-            if(success)
+            if (success)
             {
                 ViewData["Message"] = model.Players[0].SteamId;
-                ViewData["Name"] = model.Players[0].Name;
                 ViewData["Players"] = model.Players;
             }
+            ViewData["Name"] = name;
             ViewData["Error"] = model.ErrorText;
 
             return View(model.Players);
+        }
+
+        async public Task<IActionResult> Index(string name, string delayed)
+        {
+            if(!string.IsNullOrEmpty(delayed))
+                if(delayed.Equals("true"))
+                    System.Threading.Thread.Sleep(30000);
+
+            return await Search(name);
         }
     }
 }
